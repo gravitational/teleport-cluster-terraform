87eca92a26034abc77d713a99054d437104d9299
Brandon High <highb@users.noreply.github.com>
2021-10-27 11:54:14 -0700

    no count

--- node_asg.tf
+++ node_asg.tf
@@ -3,7 +3,6 @@
 // Setup for demo/testing purposes.
 resource "aws_autoscaling_group" "node" {
   name                      = "${var.cluster_name}-node"
-  count                     = 0
   max_size                  = 1000
   min_size                  = 0
   health_check_grace_period = 300
@@ -40,7 +39,7 @@ resource "aws_kms_grant" "node" {
   name              = "teleport_node"
   count             = var.ami_kms_key_arn != "" ? 1 : 0
   key_id            = var.ami_kms_key_arn
-  grantee_principal = aws_autoscaling_group.node[0].service_linked_role_arn
+  grantee_principal = aws_autoscaling_group.node.service_linked_role_arn
   operations        = ["Encrypt", "Decrypt", "ReEncryptFrom", "ReEncryptTo", "GenerateDataKey", "GenerateDataKeyWithoutPlaintext", "DescribeKey", "CreateGrant"]
   retire_on_delete  = false
 }
