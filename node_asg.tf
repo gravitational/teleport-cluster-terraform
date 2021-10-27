829292c5f2f8ede70654fc9afd838f4bf8f83b34
Brandon High <highb@users.noreply.github.com>
2021-10-27 11:01:20 -0700

    fix node kms grant (unused)

--- node_asg.tf
+++ node_asg.tf
@@ -35,11 +35,11 @@ resource "aws_autoscaling_group" "node" {
   }
 }
 
-resource "aws_kms_grant" "monitor_acm" {
-  name              = "teleport_monitor_acm"
+resource "aws_kms_grant" "node" {
+  name              = "teleport_node"
   count             = var.ami_kms_key_arn != "" ? 1 : 0
   key_id            = var.ami_kms_key_arn
-  grantee_principal = aws_autoscaling_group.monitor_acm[0].service_linked_role_arn
+  grantee_principal = aws_autoscaling_group.node[0].service_linked_role_arn
   operations        = ["Encrypt", "Decrypt", "ReEncryptFrom", "ReEncryptTo", "GenerateDataKey", "GenerateDataKeyWithoutPlaintext", "DescribeKey", "CreateGrant"]
   retire_on_delete  = false
 }
