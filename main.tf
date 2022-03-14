0b8b5d3b3f5f1c0e4bbdde877f4b0556a1fce1d5
Brandon High <highb@users.noreply.github.com>
2022-03-14 10:32:56 -0700

    remove module.teleport_cluster from move

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
 moved {
-  from = module.teleport_cluster.aws_subnet.public[1]
-  to = module.teleport_cluster.aws_subnet.public["us-west-2b"]
+  from = aws_subnet.public[1]
+  to = aws_subnet.public["us-west-2b"]
 }
