7a42d15a442f05bc3cbb05b3eee116f0939c760e
Brandon High <highb@users.noreply.github.com>
2022-03-14 10:53:22 -0700

    more moves

--- main.tf
+++ main.tf
@@ -38,3 +38,53 @@ moved {
   from = aws_route_table_association.auth[1]
   to = aws_route_table_association.auth["us-west-2b"]
 }
+
+moved {
+  from = aws_route_table.public[0]
+  to = aws_route_table.public["us-west-2a"]
+}
+
+moved {
+  from = aws_route_table.public[1]
+  to = aws_route_table.public["us-west-2b"]
+}
+
+moved {
+  from = aws_route_table.auth[0]
+  to = aws_route_table.auth["us-west-2a"]
+}
+
+moved {
+  from = aws_route_table.auth[1]
+  to = aws_route_table.auth["us-west-2b"]
+}
+
+moved {
+  from = aws_route.public_gateway[0]
+  to = aws_route.public_gateway["us-west-2a"]
+}
+
+moved {
+  from = aws_route.public_gateway[1]
+  to = aws_route.public_gateway["us-west-2b"]
+}
+
+moved {
+  from = aws_route.auth[0]
+  to = aws_route.auth["us-west-2a"]
+}
+
+moved {
+  from = aws_route.auth[1]
+  to = aws_route.auth["us-west-2b"]
+}
+
+moved {
+  from = aws_nat_gateway.teleport[0]
+  to = aws_nat_gateway.teleport["us-west-2a"]
+}
+
+moved {
+  from = aws_nat_gateway.teleport[1]
+  to = aws_nat_gateway.teleport["us-west-2b"]
+}
