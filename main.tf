609cb99638abd5f0c0a80fcddc5c61a2c9301b41
Brandon High <highb@users.noreply.github.com>
2022-03-14 10:38:16 -0700

    more moves

--- main.tf
+++ main.tf
@@ -1,5 +1,40 @@
 
 moved {
+  from = aws_subnet.public[0]
+  to = aws_subnet.public["us-west-2a"]
+}
+
+moved {
   from = aws_subnet.public[1]
   to = aws_subnet.public["us-west-2b"]
+}
+
+moved {
+  from = aws_subnet.auth[0]
+  to = aws_subnet.auth["us-west-2a"]
+}
+
+moved {
+  from = aws_subnet.auth[1]
+  to = aws_subnet.auth["us-west-2b"]
+}
+
+moved {
+  from = aws_route_table_association.public[0]
+  to = aws_route_table_association.public["us-west-2a"]
+}
+
+moved {
+  from = aws_route_table_association.public[1]
+  to = aws_route_table_association.public["us-west-2b"]
+}
+
+moved {
+  from = aws_route_table_association.auth[0]
+  to = aws_route_table_association.auth["us-west-2a"]
+}
+
+moved {
+  from = aws_route_table_association.auth[1]
+  to = aws_route_table_association.auth["us-west-2b"]
 }
