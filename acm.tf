944786c68d12feb59d8e2856124530b542c28aa0
Dima Ilchenko <dima@goteleport.com>
2022-10-28 00:52:10 -0400

    Attempt new proxy ACM validation approach

--- acm.tf
+++ acm.tf
@@ -18,13 +18,13 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = [
-  for dvo in aws_acm_certificate.cert.domain_validation_options : {
+  for_each = {
+  for dvo in aws_acm_certificate.cert[0].domain_validation_options : dvo.domain_name => {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
     type   = dvo.resource_record_type
   }
-  ]
+  }
   name            = each.value.name
   records         = [each.value.record]
   type            = each.value.type
