02bac0e2893c1bc27a5e326201f5655ccdb1a4bb
mfbnigel <52332996+mfbnigel@users.noreply.github.com>
2023-06-15 13:21:15 -0500

    Update ssm.tf to reflect the ha example

--- ssm.tf
+++ ssm.tf
@@ -3,10 +3,10 @@
 // is destroyed, cluster will overwrite them with real values
 
 resource "aws_ssm_parameter" "license" {
-  count     = var.teleport_license != "" ? 1 : 0
+  count     = var.license_path != "" ? 1 : 0
   name      = "/teleport/${var.cluster_name}/license"
   type      = "SecureString"
-  value     = var.teleport_license
+  value     = file(var.license_path)
   overwrite = true
 }
 
